217b0a1c6101c10d48532bccd6d15b5cd36f0ad4
YorickSTR <yorick@travpromobile.com>
2022-09-08 16:14:02 +0200

    Added InfoWindow when a marker is clicked

--- src/App.js
+++ src/App.js
@@ -9,8 +9,20 @@ export default () => {
   const [marker, setMarker] = useState();
 
   const fetchData = async () => {
+    let lat1 = 36.5098445064823;
+    let lat2 = 35.74337885497288;
+    let long1 = -114.83208606646728;
+    let long2 = -115.48191020892334;
     const response = await fetch(
-      "https://travpro.yourworldapps.nl/API/app/v2/listings.php?app=las-vegas&lat1=36.5098445064823&lat2=35.74337885497288&lon1=-114.83208606646728&lon2=-115.48191020892334?category=&query="
+      "https://travpro.yourworldapps.nl/API/app/v2/listings.php?app=las-vegas&lat1=" +
+        lat1 +
+        "&lat2=" +
+        lat2 +
+        "&lon1=" +
+        long1 +
+        "&lon2=" +
+        long2 +
+        "?category=&query="
     );
     if (!response.ok) {
       throw new Error("Error fetching data");
--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,5 +1,10 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 
 const containerStyle = {
   width: "50%",
@@ -14,11 +19,19 @@ const defaultCenter = {
 const Map = ({ items, marker }) => {
   const [center, setCenter] = useState(defaultCenter);
   const [zoom, setZoom] = useState(11);
+  const [activeMarker, setActiveMarker] = useState(null);
 
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: "AIzaSyBpfyrBMTrgHH3YnaGHxfjEN_w7OXBJoqc",
   });
+
+  const handleActiveMarker = (marker) => {
+    if (marker === activeMarker) {
+      return;
+    }
+    setActiveMarker(marker);
+  };
 
   useEffect(() => {
     if (marker) {
@@ -32,25 +45,42 @@ const Map = ({ items, marker }) => {
       console.log(Number(marker.longitude));
     }
   }, [marker]);
+
+  // useEffect(() => {
+  //   Map.onBoundsChanged(() => {
+  //     console.log("asd");
+  //   });
+  // });
+
   return isLoaded ? (
     <GoogleMap
       className="Map"
       mapContainerStyle={containerStyle}
       center={center}
       zoom={zoom}
+      onDrag={(e) => console.log("dragging")}
+      onClick={() => setActiveMarker(null)}
     >
       {items.map((item) => {
         let position = {
           lat: Number(item.latitude),
           lng: Number(item.longitude),
         };
+
         return (
           <Marker
             className="Marker"
             useJsApiLoader
             key={item.listing_id}
             position={position}
-          />
+            onClick={() => handleActiveMarker(item.listing_id)}
+          >
+            {activeMarker === item.listing_id ? (
+              <InfoWindow onCloseClick={() => setActiveMarker(null)}>
+                <div>{item.company}</div>
+              </InfoWindow>
+            ) : null}
+          </Marker>
         );
       })}
     </GoogleMap>
