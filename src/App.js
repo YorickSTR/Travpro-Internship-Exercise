e926f025ff50e47e07f0ae60c0c393748625ebe4
YorickSTR <yorick@travpromobile.com>
2022-09-08 13:31:04 +0200

    API call & displaying items in List

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import { React } from "react";
+import { React, useState, useEffect } from "react";
 import List from "./Components/List";
 import Map from "./Components/Map";
 import "./App.scss";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+  const [items, getItems] = useState([]);
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://travpro.yourworldapps.nl/API/app/v2/listings.php?app=las-vegas&lat1=36.5098445064823&lat2=35.74337885497288&lon1=-114.83208606646728&lon2=-115.48191020892334?category=&query="
+    );
+    if (!response.ok) {
+      throw new Error("Error fetching data");
+    } else {
+      return response.json();
+    }
+  };
+  useEffect(() => {
+    fetchData()
+      .then((res) => {
+        getItems(res);
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  }, []);
   return (
     <div className="App">
-      <List />
+      <List items={items} />
       <Map />
     </div>
   );
--- src/Components/List.js
+++ src/Components/List.js
@@ -1,9 +1,19 @@
 import { React, useState, useEffect } from "react";
 
-const List = () => {
+const List = ({ items }) => {
+  console.log(items);
   return (
     <div className="List">
-      <p>List</p>
+      <p id="amount"> Amount of results</p>
+      {items.map((item) => (
+        <div key={item.company} className="Item">
+          <p id="company"> {item.company} </p>
+          <p> {item.city} </p>
+          <p> + {item.phone} </p>
+          <p> {item.addr1} </p>
+          <button className="OnMapBtn">On Map</button>
+        </div>
+      ))}
     </div>
   );
 };
